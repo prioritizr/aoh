b4b6fd9c26e6a2b2af9ee7a514db9a03ffe11a5b
Jeff Hanson <jeffrey.hanson@uqconnect.edu.au>
2021-10-30 19:37:47 +1300

    make parallel project tests more explicit

--- tests/testthat/test_parallel_project.R
+++ tests/testthat/test_parallel_project.R
@@ -1,6 +1,6 @@
 context("parallel_project()")
 
-test_that("single layer", {
+test_that("single layer (single core)", {
   skip_on_cran()
   # create data
   x <- terra::rast(
@@ -17,22 +17,70 @@ test_that("single layer", {
   )
   y <- terra::init(y, runif)
   # create correct result
-  z <- terra::project(x, y)
+  z1 <- terra::project(x, y)
   # create results using function
-  z1 <- parallel_project(x, y, verbose = FALSE, parallel_n_threads = 1)
-  expect_equal(terra::values(z), terra::values(z1), tolerance = 1e-5)
+  z2 <- parallel_project(x, y, verbose = FALSE, parallel_n_threads = 1)
+  # tests
+  expect_true(terra::compareGeom(z1, z2, stopOnError = FALSE, res = TRUE))
+  expect_equal(terra::values(z1), terra::values(z2), tolerance = 1e-5)
+})
+
+test_that("single layer (PSOCK cluster)", {
+  skip_on_cran()
+  # create data
+  x <- terra::rast(
+    ncols = 40, nrows = 40,
+    xmin = -110, xmax = -90, ymin = 40, ymax = 60,
+    crs = "+proj=longlat +datum=WGS84"
+  )
+  x <- terra::init(x, runif)
+  names(x) <- "random"
+  y <- terra::rast(
+    ncols = 94, nrows = 124,
+    xmin = -944881, xmax = 935118, ymin = 4664377, ymax = 7144377,
+    crs = "+proj=lcc +lat_1=48 +lat_2=33 +lon_0=-100 +datum=WGS84"
+  )
+  y <- terra::init(y, runif)
+  # create correct result
+  z1 <- terra::project(x, y)
+  # create results using function
   z2 <- parallel_project(
     x, y, parallel_n_threads = 2, verbose = FALSE, parallel_cluster = "PSOCK"
   )
-  expect_equal(terra::values(z), terra::values(z2), tolerance = 1e-5)
-  skip_on_os("windows")
-  z3 <- parallel_project(
-    x, y, parallel_n_threads = 2, verbose = FALSE, parallel_cluster = "FORK"
-  )
-  expect_equal(terra::values(z), terra::values(z3), tolerance = 1e-5)
+  # tests
+  expect_true(terra::compareGeom(z1, z2, stopOnError = FALSE, res = TRUE))
+  expect_equal(terra::values(z1), terra::values(z2), tolerance = 1e-5)
 })
 
-test_that("multiple layers", {
+test_that("single layer (FORK cluster)", {
+  skip_on_cran()
+  skip_on_os("windows")
+  # create data
+  x <- terra::rast(
+    ncols = 40, nrows = 40,
+    xmin = -110, xmax = -90, ymin = 40, ymax = 60,
+    crs = "+proj=longlat +datum=WGS84"
+  )
+  x <- terra::init(x, runif)
+  names(x) <- "random"
+  y <- terra::rast(
+    ncols = 94, nrows = 124,
+    xmin = -944881, xmax = 935118, ymin = 4664377, ymax = 7144377,
+    crs = "+proj=lcc +lat_1=48 +lat_2=33 +lon_0=-100 +datum=WGS84"
+  )
+  y <- terra::init(y, runif)
+  # create correct result
+  z1 <- terra::project(x, y)
+  # create results using function
+  z2 <- parallel_project(
+    x, y, parallel_n_threads = 2, verbose = FALSE, parallel_cluster = "FORK"
+  )
+  # tests
+  expect_true(terra::compareGeom(z1, z2, stopOnError = FALSE, res = TRUE))
+  expect_equal(terra::values(z1), terra::values(z2), tolerance = 1e-5)
+})
+
+test_that("multiple layers (single core)", {
   skip_on_cran()
   # create data
   x <- terra::rast(
@@ -53,17 +101,73 @@ test_that("multiple layers", {
   )
   y <- terra::init(y, runif)
   # create correct result
-  z <- terra::project(x, y)
+  z1 <- terra::project(x, y)
   # create results using function
-  z1 <- parallel_project(x, y, verbose = FALSE, parallel_n_threads = 1)
-  expect_equal(terra::values(z), terra::values(z1), tolerance = 1e-5)
+  z2 <- parallel_project(x, y, verbose = FALSE, parallel_n_threads = 1)
+  # tests
+  expect_true(terra::compareGeom(z1, z2, stopOnError = FALSE, res = TRUE))
+  expect_equal(terra::values(z1), terra::values(z2), tolerance = 1e-5)
+})
+
+test_that("multiple layers (PSOCK cluster)", {
+  skip_on_cran()
+  # create data
+  x <- terra::rast(
+    ncols = 40, nrows = 40,
+    xmin = -110, xmax = -90, ymin = 40, ymax = 60,
+    crs = "+proj=longlat +datum=WGS84"
+  )
+  x <- terra::rast(
+    lapply(seq_len(10), function(i) {
+      terra::init(x, runif)
+    })
+  )
+  names(x) <- paste0("random", seq_len(terra::nlyr(x)))
+  y <- terra::rast(
+    ncols = 94, nrows = 124,
+    xmin = -944881, xmax = 935118, ymin = 4664377, ymax = 7144377,
+    crs = "+proj=lcc +lat_1=48 +lat_2=33 +lon_0=-100 +datum=WGS84"
+  )
+  y <- terra::init(y, runif)
+  # create correct result
+  z1 <- terra::project(x, y)
+  # create results using function
   z2 <- parallel_project(
     x, y, parallel_n_threads = 2, verbose = FALSE, parallel_cluster = "PSOCK"
   )
-  expect_equal(terra::values(z), terra::values(z2), tolerance = 1e-5)
+  # tests
+  expect_true(terra::compareGeom(z1, z2, stopOnError = FALSE, res = TRUE))
+  expect_equal(terra::values(z1), terra::values(z2), tolerance = 1e-5)
+})
+
+test_that("multiple layers (FORK cluster)", {
+  skip_on_cran()
   skip_on_os("windows")
-  z3 <- parallel_project(
+  # create data
+  x <- terra::rast(
+    ncols = 40, nrows = 40,
+    xmin = -110, xmax = -90, ymin = 40, ymax = 60,
+    crs = "+proj=longlat +datum=WGS84"
+  )
+  x <- terra::rast(
+    lapply(seq_len(10), function(i) {
+      terra::init(x, runif)
+    })
+  )
+  names(x) <- paste0("random", seq_len(terra::nlyr(x)))
+  y <- terra::rast(
+    ncols = 94, nrows = 124,
+    xmin = -944881, xmax = 935118, ymin = 4664377, ymax = 7144377,
+    crs = "+proj=lcc +lat_1=48 +lat_2=33 +lon_0=-100 +datum=WGS84"
+  )
+  y <- terra::init(y, runif)
+  # create correct result
+  z1 <- terra::project(x, y)
+  # create results using function
+  z2 <- parallel_project(
     x, y, parallel_n_threads = 2, verbose = FALSE, parallel_cluster = "FORK"
   )
-  expect_equal(terra::values(z), terra::values(z3), tolerance = 1e-5)
+  # tests
+  expect_true(terra::compareGeom(z1, z2, stopOnError = FALSE, res = TRUE))
+  expect_equal(terra::values(z1), terra::values(z2), tolerance = 1e-5)
 })
