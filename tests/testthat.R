d08dee89203ebc2f57d5024942e9cdeaaee0fb6f
Jeffrey Hanson <jeffrey.hanson@uqconnect.edu.au>
2022-07-28 15:49:12 +1200

    show timings for ci tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -5,5 +5,12 @@ library(aoh)
 # enable parallel testing
 Sys.unsetenv("R_TESTS")
 
+# determine reporter
+if (identical(Sys.getenv("CI"), "true")) {
+  reporter = "progress"
+} else {
+  reporter = testthat::check_reporter()
+}
+
 # run tests
-test_check("aoh")
+test_check("aoh", reporter = reporter)
