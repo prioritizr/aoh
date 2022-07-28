5d5ac74a14fece23d8614c844e3dcbbe8722e54a
Jeffrey Hanson <jeffrey.hanson@uqconnect.edu.au>
2022-07-29 09:09:06 +1200

    make zenodo queries more robust

--- R/get_zenodo_data.R
+++ R/get_zenodo_data.R
@@ -7,20 +7,29 @@
 #'
 #' @param x `character` DOI value.
 #'
-#' @param file `character` file name for desired file.
+#' @param file `character` File name for desired file.
 #'  Alternatively, the argument can be a function that returns `TRUE` or
 #'  `FALSE` if a `character` file name is detected.
+#'
+#' @param n_attempts `integer` Number of times to attempt to query
+#'  the Zenodo API. This is useful because the Zenodo API sometimes
+#'  randomly returns internal server errors for valid requests.
+#'  Note that the function will wait for 5 seconds between subsequent
+#'  attempts.
+#'  Defaults to 5.
 #'
 #' @inherit download_habitat_data references details
 #'
 #' @return A `character` value indicating the DOI.
 #'
 #' @noRd
-latest_zenodo_version <- function(x, file) {
+latest_zenodo_version <- function(x, file, n_attempts = 5) {
   # assert valid argument
   assertthat::assert_that(
     assertthat::is.string(x),
-    assertthat::noNA(x)
+    assertthat::noNA(x),
+    assertthat::is.count(n_attempts),
+    assertthat::noNA(n_attempts)
   )
   if (!is.function(file)) {
     assertthat::assert_that(
@@ -36,14 +45,16 @@ latest_zenodo_version <- function(x, file) {
 
   # find all versions
   invisible(utils::capture.output({
-    all_versions <- suppressWarnings(z$getRecordByDOI(x)$getVersions())
+    all_versions <- suppressWarnings(
+      get_zenodo_record(x, z, n_attempts = n_attempts)$getVersions()
+    )
   }))
 
   # find version with file present
   version <- NA_character_
   for (x in rev(all_versions$doi)) {
     ## find files present in repository
-    f <- z$getRecordByDOI(x)$listFiles()$filename
+    f <- get_zenodo_record(x, z, n_attempts = n_attempts)$listFiles()$filename
     ## verify that has zip file following expected pattern
     if (inherits(file, "function")) {
       file_found <- any(vapply(f, file, logical(1)))
@@ -79,7 +90,7 @@ latest_zenodo_version <- function(x, file) {
 #' @return A `character` file path for the downloaded data.
 #'
 #' @noRd
-get_zenodo_data <- function(x, file, dir = tempdir(),
+get_zenodo_data <- function(x, file, dir = tempdir(), n_attempts = 5,
                             force = FALSE, verbose = TRUE) {
   # assert valid argument
   assertthat::assert_that(
@@ -88,7 +99,9 @@ get_zenodo_data <- function(x, file, dir = tempdir(),
     assertthat::is.string(dir),
     assertthat::noNA(dir),
     assertthat::is.flag(verbose),
-    assertthat::noNA(verbose)
+    assertthat::noNA(verbose),
+    assertthat::is.count(n_attempts),
+    assertthat::noNA(n_attempts)
   )
   if (!is.function(file)) {
     assertthat::assert_that(
@@ -119,7 +132,7 @@ get_zenodo_data <- function(x, file, dir = tempdir(),
   z <- zen4R::ZenodoManager$new(logger = NULL)
 
   # find all files in record
-  f <- z$getRecordByDOI(x)$listFiles()
+  f <- get_zenodo_record(x, z, n_attempts = n_attempts)$listFiles()
 
   # find file to download
   if (inherits(file, "function")) {
@@ -143,4 +156,61 @@ get_zenodo_data <- function(x, file, dir = tempdir(),
     }
     p
   }))
+}
+
+#' Get Zenodo Record
+#'
+#' Get metadata for a Zenodo record using the Zenodo API.
+#'
+#' @param x `character` DOI.
+#'
+#' @param z `ZenodoManager` Zenodo manager object.
+#'   Defaults to `zen4R::ZenodoManager$new(logger = NULL)`.
+#'
+#' @inheritParams latest_zenodo_version
+#'
+#' @return The result from `zen4R::ZenodoManager$getRecordByDOI()`.
+#'
+#' @noRd
+get_zenodo_record <- function(x, z = zen4R::ZenodoManager$new(logger = NULL),
+                  n_attempts = 5) {
+  # assert valid arguments
+  assertthat::assert_that(
+    assertthat::is.string(x),
+    assertthat::noNA(x),
+    inherits(z, "ZenodoManager"),
+    assertthat::is.count(n_attempts),
+    assertthat::noNA(n_attempts)
+  )
+
+  # attempt to query API
+  r <- NULL
+  for (i in seq_len(n_attempts)) {
+    # try to query result
+    r <- try(z$getRecordByDOI(x), silent = TRUE)
+    # if success, then exit now
+    if (!inherits(r, "try-error")) {
+      break
+    }
+    # if the error is not due to internal server error, then exit too
+    if (
+      any(
+        grepl("$ operator is invalid for atomic vectors", r[[1]], fixed = TRUE)
+      )
+    ) {
+      stop(r[[1]])
+    }
+    # otherwise, wait for 5 seconds and try again
+    if (i < n_attempts) {
+      Sys.sleep(5)
+    }
+  }
+
+  # if the result is an error after trying multiple times, then throw error
+  if (inherits(r, "try-error")) {
+    stop("failed to access Zenodo API")
+  }
+
+  # return result
+  r
 }
