f1eb1b1b2dfb1b2ea72be28e8ea331a1c92ec3c3
Jeffrey Hanson <jeffrey.hanson@uqconnect.edu.au>
2022-07-28 17:19:52 +1200

    remove tests to reduce timings

--- tests/testthat/test_get_jung_lvl2_habitat_data.R
+++ tests/testthat/test_get_jung_lvl2_habitat_data.R
@@ -42,36 +42,13 @@ test_that("latest version (from cache)", {
   expect_equal(terra::nlyr(x), 1)
 })
 
-test_that("specified version (from online)", {
+test_that("specified version", {
   # skip if needed
   skip_on_cran()
   skip_if_offline()
   skip_if_local_and_slow_internet()
   # create object
   x <- get_jung_lvl2_habitat_data(
-    version = latest_jung_version,
-    force = TRUE,
-    verbose = interactive()
-  )
-  # tests
-  expect_is(x, "SpatRaster")
-  expect_true(sf::st_crs(terra::crs(x)) == sf::st_crs("ESRI:54017"))
-  expect_lte(terra::xmin(x), -17367531)
-  expect_gte(terra::xmax(x), 17367569)
-  expect_lte(terra::xmin(x), -6005523)
-  expect_gte(terra::ymax(x), 7287077)
-  expect_equal(terra::nlyr(x), 1)
-})
-
-test_that("specified version (from cache)", {
-  # skip if needed
-  skip_on_cran()
-  skip_if_offline()
-  skip_if_not_installed("rappdirs")
-  skip_if_cached_data_not_available()
-  # create object
-  x <- get_jung_lvl2_habitat_data(
-    dir = rappdirs::user_data_dir("aoh"),
     version = latest_jung_version,
     force = FALSE,
     verbose = interactive()
